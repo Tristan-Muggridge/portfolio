0829569652e52fc4bd45ca135a76c857e7f1cf33
Tristan Muggridge <89533155+Tristan-Muggridge@users.noreply.github.com>
2023-02-13 12:26:22 +1000

    adding additional disabled param

--- src/components/sub-components/Button.tsx
+++ src/components/sub-components/Button.tsx
@@ -1,10 +1,16 @@
 interface IProps {
     buttonText: string
+    disabled?: boolean
     // onClick(): void
 }
 
-export default function ({buttonText}: IProps) {
+export default function ({buttonText, disabled}: IProps) {
     return (
-        <button type="submit" className='outline outline-neutral-300 rounded-full px-6 my-8 text-neutral-300 w-fit mx-auto'>{buttonText}</button>
+        <button 
+        disabled={!disabled}
+        type="submit" 
+        className={`outline rounded-full px-6 my-8 w-fit mx-auto ${disabled == false ? 'outline-red-100 text-red-100' : 'outline-green-100 text-green-100'}`}>
+            {buttonText}
+        </button>
     )
 }
